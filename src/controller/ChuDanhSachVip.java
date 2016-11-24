6ab49c90be7a5cad4e540531c984b40cd2d1a1d7
leductienttkt <leductienttkt@gmail.com>
2016-11-25 00:08:34 +0700

    ngủ

---
+++ src/controller/ChuDanhSachVip.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Thue;
+import bean.Vip;
+import bo.DanhSachThueBO;
+import bo.DanhSachVipBO;
+
+/**
+ * Servlet implementation class ChuDanhSachVip
+ */
+@WebServlet("/ChuDanhSachVip")
+public class ChuDanhSachVip extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ChuDanhSachVip() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<Vip> list = null;
+		list = DanhSachVipBO.getVipList("aa");
+		request.setAttribute("danhSachVip", list);
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/chu/chuvip.jsp");  
+	    dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
